fe67e69c82dfcf3336b5a6633ece91f60405041b
nikoren <nikolai.orenstrakh@stratoscale.com>
2019-03-27 22:05:07 -0400

    Add views/bootstrap

--- main.go
+++ main.go
@@ -1,16 +1,80 @@
 package main
 
 import (
+	"gallery/views"
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
 	"headers"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	w.Header().Set(headers.ContentTypeTextHtml())
-	w.Write([]byte("<h1>JOPA</jopa>"))
+const (
+	Port = 3000
+	LayoutsDir = "views/layouts"
+)
+
+var (
+	homeView    *views.View
+	contactView *views.View
+	notFoundView *views.View
+	sighnUpView *views.View
+)
+
+func handleIfErr(msg string, err error ){
+	if err != nil{
+		log.Fatalf("MSG: %s, ERROR: %s", msg, err)
+	}
 }
 
-func main(){
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+func init(){
+	// setup logging
+	log.SetFormatter(&log.TextFormatter{})
+	log.SetLevel(log.DebugLevel)
+	log.SetReportCaller(true)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Home requested")
+	w.Header().Set(headers.ContentTypeTextHtml())
+	handleIfErr("Couldn't render view", homeView.Render(w, nil))
+}
+
+func contacHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Contact requested")
+	w.Header().Set(headers.ContentTypeTextHtml())
+	handleIfErr("Couldn't render contact view", contactView.Render(w, nil))
+}
+
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	log.Debug("Signup requested")
+	w.Header().Set(headers.ContentTypeTextHtml())
+	handleIfErr("Couldn't render signup view", sighnUpView.Render(w, nil))
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	log.Warn("Unknown path was requested: ", r.URL.Path)
+	w.WriteHeader(http.StatusNotFound)
+	handleIfErr("Couldn't render 404", notFoundView.Render(w, nil))
+}
+
+func main() {
+	// Temp
+	homeView = views.NewView(
+		"bootstrap", LayoutsDir, "views/home.gohtml")
+	contactView = views.NewView(
+		"bootstrap", LayoutsDir,"views/contact.gohtml")
+	notFoundView = views.NewView(
+		"bootstrap", LayoutsDir,"views/404.gohtml")
+	sighnUpView = views.NewView(
+		"bootstrap", LayoutsDir,"views/signup.gohtml")
+
+	// Routing
+	r := mux.Router{}
+	r.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	r.HandleFunc("/home", homeHandler)
+	r.HandleFunc("/contact", contacHandler)
+	r.HandleFunc("/signup", signUpHandler)
+
+	// Serving
+	handleIfErr("Coudn't start server", http.ListenAndServe(":3000", &r))
 }
---
+++ views/view.go
@@ -0,0 +1,53 @@
+package views
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"html/template"
+	"io"
+	"path/filepath"
+)
+
+// NewView - creates new view by searching for all layouts in layoutsDir , combining them
+// with the provided  contentTemplates , parses the combined list and setting them as
+// ContentTemplate, also you should pick the name of the template that will be wrapping the
+// ContentTemplate by providing layoutName
+func NewView(layoutName string, layoutsDir string,  contentTemplates ...string) *View {
+
+	// find all layouts
+		foundLayouts, err := filepath.Glob(
+			fmt.Sprintf("%s/*.gohtml",layoutsDir)) // (matches []string, err error)
+		if err != nil{
+			log.Fatalf("Couldn't glob %s, ERROR: %v", layoutsDir, err.Error())
+		}
+
+	// aggregate all templates
+	contentTemplates = append(contentTemplates, foundLayouts...)
+
+	// parse all templates
+	parsedContentTemplates, err := template.ParseFiles(contentTemplates...)
+	if err != nil{
+		log.Panicf("Couldn't parse templates, ERROR: %s", err )
+	}
+
+	// create view and set the main template to render(LayoutName)
+	v := View{
+		LayoutName:      layoutName,
+		ContentTemplate: parsedContentTemplates,
+	}
+	return &v
+}
+
+type View struct {
+	LayoutName      string
+	ContentTemplate *template.Template
+}
+
+func(v *View)Render(w io.Writer, data interface{}) error{
+	err := v.ContentTemplate.ExecuteTemplate(w, v.LayoutName, data)
+	if err != nil{
+		log.Errorf("Couldn't execute template correctly, ERROR: %s", err.Error())
+	}
+	return err
+}
+
